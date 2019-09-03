90aa4ea96bd0e34d2b6d59185ce745737af48236
Leroux Romain <romain.leroux@streamroot.io>
2019-09-03 12:50:14 +0200

    Add doc

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # helm-fd
+
+[![MELPA](https://melpa.org/packages/helm-fd-badge.svg)](https://melpa.org/#/helm-fd)
+[![License](http://img.shields.io/:license-gpl3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0.html)
+
+Same as [helm-find][] but using [fd][] instead of `find`, therefore
+you have to install it beforehand. `fd` is written in Rust and is much
+faster than `find`.
+
+The main function is `helm-fd` and mimics `helm-find`. A project
+scoped search function `helm-fd-project` is also provided. By default,
+`fd` will ignore hidden directories/files and patterns from
+`.gitignore` file when present.
+
+## Setup
+
+``` emacs-lisp
+(use-package helm-fd
+  :bind (:map helm-command-map
+              ("/" . helm-fd)))
+```
+
+[helm-find]: https://github.com/emacs-helm/helm/blob/master/helm-find.el
+[fd]: https://github.com/sharkdp/fd
--- helm-fd.el
+++ helm-fd.el
@@ -1,27 +1,33 @@
-;;; helm-fd.el --- helm interface for fd command. -*- lexical-binding: t -*-
+;;; helm-fd.el --- Helm interface for fd command. -*- lexical-binding: t -*-
 
 ;; Author: Romain Leroux <romain@leroux.dev>
 ;; URL: https://github.com/lerouxrgd/helm-fd
-;; Version: 0.1
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "26.2") (helm "3.3"))
 ;; Keywords: fd, find, files, helm, fast
 
+;; This file is not part of GNU Emacs.
+
 ;;; License:
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see
+;; <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+
+;;; Same as `helm-find' but using fd instead of find.  Also provides a
+;;; project scoped search function `helm-fd-project'.
 
 ;;; Code:
 
@@ -32,8 +38,13 @@
   "Group for `helm-fd' customizations."
   :group 'helm)
 
-(defcustom helm-fd-bin (executable-find "fd")
-  "The location of the fd binary executable."
+(defcustom helm-fd-cmd "fd"
+  "The name to use for finding the fd executable binary."
+  :group 'helm-fd
+  :type 'string)
+
+(defcustom helm-fd-bin (executable-find helm-fd-cmd)
+  "The location of the fd executable binary."
   :group 'helm-fd
   :type 'string)
 
@@ -53,7 +64,7 @@
     (define-key map (kbd "DEL") 'helm-delete-backward-no-update)
     map))
 
-(defvar helm-source-fd
+(defvar helm-fd-source
   (helm-build-async-source "fd"
     :header-name (lambda (name)
                    (concat name " in [" (helm-default-directory) "]"))
@@ -68,6 +79,7 @@
     :requires-pattern 3))
 
 (defun helm-fd-transformer (candidates _source)
+  "A helm transformer for CANDIDATES."
   (let (non-essential
         (default-directory (helm-default-directory)))
     (cl-loop for i in candidates
@@ -92,6 +104,7 @@
                                     abs))))))
 
 (defun helm-fd-shell-command-fn ()
+  "Run and supervise the fd subprocess."
   (let* (process-connection-type
          non-essential
          (cmd (concat (helm-fd--build-cmd-line)
@@ -120,10 +133,10 @@
                      (replace-regexp-in-string "\n" "" event))))))))
 
 (defun helm-fd--build-cmd-line ()
+  "Build the fd command line from current `helm-pattern'."
   (let* ((default-directory (or (file-remote-p default-directory 'localname)
                                 default-directory))
          (patterns+options (split-string helm-pattern "\\(\\`\\| +\\)\\* +"))
-         (fold-case (helm-set-case-fold-search (car patterns+options)))
          (patterns (split-string (car patterns+options)))
          (additional-options (and (cdr patterns+options)
                                   (list (concat (cadr patterns+options) " ")))))
@@ -133,14 +146,22 @@
             (string-join additional-options " "))))
 
 (defun helm-fd-1 (dir)
+  "Setup helm fd source and start it from DIR."
   (let ((default-directory (file-name-as-directory dir)))
-    (helm :sources 'helm-source-fd
+    (helm :sources 'helm-fd-source
           :buffer "*helm fd*"
           :ff-transformer-show-only-basename nil
           :case-fold-search helm-file-name-case-fold-search)))
 
 ;;;###autoload
 (defun helm-fd-project ()
+  "Preconfigured `helm-fd' for search within the current project.
+
+If the current file isn't part of a VCS project,
+`default-directory' will be the location where the current Emacs
+process was started.
+
+Also note that the displayed paths are relative to project root."
   (interactive)
   (let ((directory
          (or (cdr (project-current))
@@ -150,6 +171,18 @@
 
 ;;;###autoload
 (defun helm-fd (arg)
+  "Preconfigured `helm' for the fd shell command.
+
+Recursively find files whose names are matched by all specified
+globbing PATTERNs under the current directory using the external
+program \"fd\" specified in `helm-fd-cmd'.  Input PATTERNs are
+interleaved with \".*?\".
+
+With prefix argument ARG, prompt for a directory to search.
+
+The list of globbing PATTERNs can be followed by the separator
+\"*\" plus any number of additional arguments that are passed to
+\"fd\" literally."
   (interactive "P")
   (let ((directory
          (if arg
